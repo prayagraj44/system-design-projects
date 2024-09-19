257d6df4cb27ac197555d701b2b0a9c3e24206d6
prayag <prayagraj44@gmail.com>
2024-09-20 00:17:15 +0530

    skeleton app url shortener

---
+++ url-shortener/src/test/java/com/checkwithprayag/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkwithprayag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
